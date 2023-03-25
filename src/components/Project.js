d3b2726e9527622848516464567dc13733464a60
jpinto3 <jpinto3@mail.csuchico.edu>
2023-03-24 20:51:29 -0700

    project component

---
+++ src/components/Project.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Project({ project }) {
+
+  const { name, repo, deployment, picture } = project;
+
+  return (
+    <div className="container-fluid d-flex flex-column bg-info text-white" key={name}>
+      <img
+        src={require(`${picture}.png`).default}
+        alt={name}
+        width="300" height="300"> 
+        </img>
+      <div>
+        <h3>
+          <a href={link}>{name}</a>{' '}
+          <a href={repo}>Github Repo</a>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
